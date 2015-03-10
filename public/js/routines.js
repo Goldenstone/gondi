130bfc3c469ac18a8dbeec423d826bd8df92d7b6
Allen Wu <allenwu1018@gmail.com>
2015-03-10 20:12:25 +0800

    add destination condition

--- public/js/routines.js
+++ public/js/routines.js
@@ -3970,18 +3970,54 @@ var _ = require('underscore');
 var makeRoutines = function(options) {
   if (!options.root) throw Error("missing root!");
 
+  // namespace
   var routines = {};
 
+  // view-model
   routines.vm = {};
   routines.vm.init = function() {
+    var vm = routines.vm;
+
     this.routines = m.request({ method: 'GET', url: '/api/routines/' }); 
+    
+    // Conditions
+    this.toCond = m.prop(null);
+    //this.typeCond = m.prop(null);
+    this.filters = m.prop([
+      function(r) {
+        return vm.toCond() === null || vm.toCond() === r.to; 
+      },
+      /*function(r) {
+        return vm.typeCond
+      }*/
+    ]);
+
+    this.changeCond = function(e) {
+      var condType = this + 'Cond'
+        , value = e.target.value;
+
+      if (vm.hasOwnProperty(condType)) {
+        vm[condType](value?value:null);
+      }
+    }
+
+    this.filteredRoutines = function() {
+      return _.filter(this.routines(), function(r) {
+        var filters = this.filters();
+        for(var i = 0; i < filters.length; i++) {
+          if (!filters[i].apply(this, [r])) return false;
+        }
+        return true;
+      }, this);
+    };
   };
 
+  // view
   routines.view = function(ctrl) {
     var vm = routines.vm;
 
     var getTableHeaders = function() {
-      var headers = ['发车地点', '终点站', '价格', '发车时间', '备注'];
+      var headers = ['发车地点', '目的地', '价格', '发车时间', '备注'];
       return _.map(headers, function(h) {
         return m('th', h); 
       });
@@ -3997,15 +4033,39 @@ var makeRoutines = function(options) {
         ]);
       });
     };
+    
+    var getRoutineTable = function(routines) {
+      return m('table.pure-table.pure-table-bordered', [
+        m('thead', [
+          m('tr', getTableHeaders())
+        ]),
+        m('tbody', getRoutines(routines))
+      ]);
+    };
+    
+    var getControlSection = function() {
+      var destinations = _.uniq(_.pluck(vm.routines(), 'to'));
+      destinations.unshift(null);
 
-    return m('table.pure-table.pure-table-bordered', [
-      m('thead', [
-        m('tr', getTableHeaders())
-      ]),
-        m('tbody', getRoutines(vm.routines()))
-    ]);
+      return m('.controls', [
+        m('.control', [
+          m('span', '目的地'),
+          m('select', { onchange: vm.changeCond.bind('to') }, _.map(destinations, function(dest) {
+            if (dest === null)
+              return m('option', { value: '' }, '无');
+            return m('option', { value: dest }, dest);
+          }))
+        ])
+      ]);
+    }
+
+    return [
+      getControlSection(),
+      getRoutineTable(vm.filteredRoutines())
+    ]
   };
 
+  // controller
   routines.controller = function() {
     var vm = routines.vm;
     vm.init();
@@ -4019,7 +4079,7 @@ makeRoutines({
   root: document.getElementById('routines')
 });
 
-}).call(this,require("oMfpAn"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_43f6e69f.js","/")
+}).call(this,require("oMfpAn"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_972c9a7b.js","/")
 },{"buffer":1,"mithril":5,"oMfpAn":4,"underscore":6}]},{},[7])
 
 
--- src/js/routines.js
+++ src/js/routines.js
@@ -4,18 +4,54 @@ var _ = require('underscore');
 var makeRoutines = function(options) {
   if (!options.root) throw Error("missing root!");
 
+  // namespace
   var routines = {};
 
+  // view-model
   routines.vm = {};
   routines.vm.init = function() {
+    var vm = routines.vm;
+
     this.routines = m.request({ method: 'GET', url: '/api/routines/' }); 
+    
+    // Conditions
+    this.toCond = m.prop(null);
+    //this.typeCond = m.prop(null);
+    this.filters = m.prop([
+      function(r) {
+        return vm.toCond() === null || vm.toCond() === r.to; 
+      },
+      /*function(r) {
+        return vm.typeCond
+      }*/
+    ]);
+
+    this.changeCond = function(e) {
+      var condType = this + 'Cond'
+        , value = e.target.value;
+
+      if (vm.hasOwnProperty(condType)) {
+        vm[condType](value?value:null);
+      }
+    }
+
+    this.filteredRoutines = function() {
+      return _.filter(this.routines(), function(r) {
+        var filters = this.filters();
+        for(var i = 0; i < filters.length; i++) {
+          if (!filters[i].apply(this, [r])) return false;
+        }
+        return true;
+      }, this);
+    };
   };
 
+  // view
   routines.view = function(ctrl) {
     var vm = routines.vm;
 
     var getTableHeaders = function() {
-      var headers = ['发车地点', '终点站', '价格', '发车时间', '备注'];
+      var headers = ['发车地点', '目的地', '价格', '发车时间', '备注'];
       return _.map(headers, function(h) {
         return m('th', h); 
       });
@@ -31,15 +67,39 @@ var makeRoutines = function(options) {
         ]);
       });
     };
+    
+    var getRoutineTable = function(routines) {
+      return m('table.pure-table.pure-table-bordered', [
+        m('thead', [
+          m('tr', getTableHeaders())
+        ]),
+        m('tbody', getRoutines(routines))
+      ]);
+    };
+    
+    var getControlSection = function() {
+      var destinations = _.uniq(_.pluck(vm.routines(), 'to'));
+      destinations.unshift(null);
 
-    return m('table.pure-table.pure-table-bordered', [
-      m('thead', [
-        m('tr', getTableHeaders())
-      ]),
-        m('tbody', getRoutines(vm.routines()))
-    ]);
+      return m('.controls', [
+        m('.control', [
+          m('span', '目的地'),
+          m('select', { onchange: vm.changeCond.bind('to') }, _.map(destinations, function(dest) {
+            if (dest === null)
+              return m('option', { value: '' }, '无');
+            return m('option', { value: dest }, dest);
+          }))
+        ])
+      ]);
+    }
+
+    return [
+      getControlSection(),
+      getRoutineTable(vm.filteredRoutines())
+    ]
   };
 
+  // controller
   routines.controller = function() {
     var vm = routines.vm;
     vm.init();
