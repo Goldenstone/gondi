190b4314ba624c24a0d34529d5bc5fac1de2a94b
Allen Wu <allenwu1018@gmail.com>
2015-03-13 18:06:13 +0800

    fix

--- routes/videos.js
+++ routes/videos.js
@@ -5,7 +5,9 @@ var fs = require('fs');
 app.get('/', function(req, res) {
   var MEDIA_PATH = process.env.MEDIA_PATH || './public/videos';
   fs.readdir(MEDIA_PATH, function(err, files) {
+    files = files || [];
     var validFileNames = [];
+    console.log(files);
     files.forEach(function(name) {
       var tmp = name.split('.');
       var type = tmp[tmp.length-1]; 
@@ -15,6 +17,6 @@ app.get('/', function(req, res) {
     });
     res.render('videos', { files: validFileNames });
   });
-};
+});
 
 module.exports = app;
